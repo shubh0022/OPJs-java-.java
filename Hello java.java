079ae89a4342b420e5fe93fedd440a43696c3b8e
YADAV SHUBHAM JANGBAHADUR <153888821+shubh0022@users.noreply.github.com>
2024-12-14 23:15:04 +0530

    Create Hello java.java

---
+++ Hello java.java
@@ -0,0 +1,8 @@
+// This is the declaration of the Main class, which is the entry point of the program.
+public class Main {
+// This is the main method, where the program starts execution.
+public static void main(String[] args) {
+// This line prints the message "Hello, World!"to the console
+System.out.println("Hello, World!");
+}
+}
